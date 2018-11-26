7c5fbd477c941114d684dc65f2d69fd943609eba
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-26 13:24:44 -0500

    start main

---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main">
+    </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue'
+
+export default {
+  name: 'main',
+  components: {
+    Banner
+  }
+}
+</script>
