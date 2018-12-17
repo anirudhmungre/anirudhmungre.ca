1855c61050620e04ec002c86099c78d7d1eb0a32
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-12-17 14:52:56 -0500

    do scrolling shadow animation later

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,10 @@
 let body = document.querySelector("body")
+
 console.log(body)
 body.onscroll = function(){
+    // if (){
+
+    // }
     let stylesheet = document.styleSheets[1]
-    stylesheet.cssRules[2].style.backgroundColor="white"//"0px 20px 10px white"
+    stylesheet.cssRules[2].style.boxShadow="0px 20px 10px #5c5c5c"
 }
