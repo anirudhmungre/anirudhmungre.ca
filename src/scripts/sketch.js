158a66d79dc9372ffdf88de2a8c7ba92790698bd
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-12-17 21:36:40 -0500

    particle title

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -1,22 +1,35 @@
-let headOffset = 90
+const headOffset = 89
+const title = "ANIRUDH MUNGRE"
+const fontSize = 150
+var font
 
 function preload(){
-    
+    font = loadFont("../Resources/Roboto-Bold.ttf");
 }
 
 function setup(){
-    let sketchCanvas = createCanvas(windowWidth, windowHeight-headOffset)
-    sketchCanvas.parent("myCanvas")
-    textSize(128)
-    fill(255)
-    noStroke()
-    text("ANIRUDH", 10, 200)
+    let myCanvas = createCanvas(windowWidth, windowHeight-headOffset)
+    myCanvas.parent("myCanvas")
+    textSize(fontSize)
+    setPoints()
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight-headOffset)
+    clear()
+    resizeCanvas(windowWidth, windowHeight - headOffset);
+    setPoints()
 }
 
-function draw(){
-    background("#1f1f1f")
+function setPoints(){
+    let bounds, x, y, points, pt
+    bounds = font.textBounds(title)
+    x = windowWidth / 2 - bounds.w / 2
+    y = windowHeight / 2 - bounds.h / 2 //- headOffset
+    points = font.textToPoints(title, x, y, fontSize)
+    for (let i = 0; i < points.length; i++){
+        pt = points[i]
+        stroke(175, 23, 23)
+        strokeWeight(10)
+        point(pt.x, pt.y)
+    }
 }
