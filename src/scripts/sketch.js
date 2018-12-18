3cec747d4942b08c4dbf14816e8987477f3b70e2
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-12-18 15:30:07 -0500

    rename and titleing change back

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -8,28 +8,34 @@ function preload(){
 }
 
 function setup(){
-    let myCanvas = createCanvas(windowWidth, windowHeight-headOffset)
+    let myCanvas, bounds
+    myCanvas = createCanvas(windowWidth, windowHeight-headOffset)
     myCanvas.parent("myCanvas")
     textSize(fontSize)
-    setPoints()
+    textFont(font)
+    textColor()
+    bounds = font.textBounds(title)
+    textAlign(CENTER, CENTER)
+    text(title)
+    // setPoints()
 }
 
 function windowResized() {
     clear()
     resizeCanvas(windowWidth, windowHeight - headOffset);
-    setPoints()
+    // setPoints()
 }
 
-function setPoints(){
-    let bounds, x, y, points, pt
-    bounds = font.textBounds(title)
-    x = windowWidth / 2 - bounds.w / 2
-    y = windowHeight / 2 - bounds.h / 2 //- headOffset
-    points = font.textToPoints(title, x, y, fontSize)
-    for (let i = 0; i < points.length; i++){
-        pt = points[i]
-        stroke(175, 23, 23)
-        strokeWeight(10)
-        point(pt.x, pt.y)
-    }
-}
+// function setPoints(){
+//     let bounds, x, y, points, pt
+//     bounds = font.textBounds(title)
+//     x = windowWidth / 2 - bounds.w / 2
+//     y = windowHeight / 2 - bounds.h / 2 //- headOffset
+//     points = font.textToPoints(title, x, y, fontSize)
+//     for (let i = 0; i < points.length; i++){
+//         pt = points[i]
+//         stroke(175, 23, 23)
+//         strokeWeight(10)
+//         point(pt.x, pt.y)
+//     }
+// }
