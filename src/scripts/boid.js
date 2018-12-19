778b84af87a54aa1505ef6b63538bbe292084aa9
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-12-19 16:44:50 -0500

    refactor

--- src/scripts/boid.js
+++ src/scripts/boid.js
@@ -7,26 +7,70 @@ class Boid {
         this.maxSpeed = 4
         this.maxForce = 1
         // Implement below later to be random
-        this.percepRad = 40
+        this.percepRad = 50
         this.mass = 8
     }
 
     flock(boids){
         let steerForce = createVector()
-        steerForce.add(this.align(boids))
-        steerForce.add(this.cohesion(boids))
-        steerForce.add(this.seperation(boids))
+        steerForce = this.acs(boids)
+        // steerForce.add(this.align(boids))
+        // steerForce.add(this.cohesion(boids))
+        // steerForce.add(this.seperation(boids))
 
         // steerForce.div(this.mass)
         this.acc.add(steerForce)
     }
 
-    // acs(boids){
+    acs(boids){
+        let dis
+        let total = 0
+        let steerForceT = createVector() // Total steer force
+        let steerForceA = createVector() // Steer force of alignment
+        let steerForceC = createVector() // Steer force of cohesion
+        let steerForceS = createVector() // Steer force of seperation
+        for(let oBoid of boids){
+            dis = dist(this.pos.x, this.pos.y, oBoid.pos.x, oBoid.pos.y)
+            if ((oBoid != this) && (dis < this.percepRad)){
+                steerForceA.add(oBoid.vel)
+
+                steerForceC.add(oBoid.pos)
+
+                let diff = p5.Vector.sub(this.pos, oBoid.pos)
+                diff.div(dis)
+                steerForceS.add(diff)
+
+                total++
+            }
+        }
+        if (total > 0){
+            steerForceA.div(total)
+            steerForceA.setMag(this.maxSpeed)
+            steerForceA.sub(this.vel)
+            steerForceA.limit(this.maxForce)
+
+            steerForceC.div(total)
+            steerForceC.sub(this.pos)
+            steerForceC.setMag(this.maxSpeed)
+            steerForceC.sub(this.vel)
+            steerForceC.limit(this.maxForce)
+
+            steerForceS.div(total)
+            steerForceS.setMag(this.maxSpeed)
+            steerForceS.sub(this.vel)
+            steerForceS.limit(this.maxForce)
+        }
+
+        steerForceT.add(steerForceA)
+        steerForceT.add(steerForceC)
+        steerForceT.add(steerForceS)
+        return steerForceT
+    }
+
+    // align(boids){
     //     let dis, steerForce
     //     let total = 0
-    //     steerForceA = createVector() // Steer force of alignment
-    //     steerForceC = createVector() // Steer force of cohesion
-    //     steerForceS = createVector() // Steer force of seperation
+    //     steerForce = createVector()
     //     for(let oBoid of boids){
     //         dis = dist(this.pos.x, this.pos.y, oBoid.pos.x, oBoid.pos.y)
     //         if ((oBoid != this) && (dis < this.percepRad)){
@@ -43,68 +87,48 @@ class Boid {
     //     return steerForce
     // }
 
-    align(boids){
-        let dis, steerForce
-        let total = 0
-        steerForce = createVector()
-        for(let oBoid of boids){
-            dis = dist(this.pos.x, this.pos.y, oBoid.pos.x, oBoid.pos.y)
-            if ((oBoid != this) && (dis < this.percepRad)){
-                steerForce.add(oBoid.vel)
-                total++
-            }
-        }
-        if (total > 0){
-            steerForce.div(total)
-            steerForce.setMag(this.maxSpeed)
-            steerForce.sub(this.vel)
-            steerForce.limit(this.maxForce)
-        }
-        return steerForce
-    }
+    // cohesion(boids){
+    //     let dis, steerForce
+    //     let total = 0
+    //     steerForce = createVector()
+    //     for(let oBoid of boids){
+    //         dis = dist(this.pos.x, this.pos.y, oBoid.pos.x, oBoid.pos.y)
+    //         if ((oBoid != this) && (dis < this.percepRad)){
+    //             steerForce.add(oBoid.pos)
+    //             total++
+    //         }
+    //     }
+    //     if (total > 0){
+    //         steerForce.div(total)
+    //         steerForce.sub(this.pos)
+    //         steerForce.setMag(this.maxSpeed)
+    //         steerForce.sub(this.vel)
+    //         steerForce.limit(this.maxForce)
+    //     }
+    //     return steerForce
+    // }
 
-    cohesion(boids){
-        let dis, steerForce
-        let total = 0
-        steerForce = createVector()
-        for(let oBoid of boids){
-            dis = dist(this.pos.x, this.pos.y, oBoid.pos.x, oBoid.pos.y)
-            if ((oBoid != this) && (dis < this.percepRad)){
-                steerForce.add(oBoid.pos)
-                total++
-            }
-        }
-        if (total > 0){
-            steerForce.div(total)
-            steerForce.sub(this.pos)
-            steerForce.setMag(this.maxSpeed)
-            steerForce.sub(this.vel)
-            steerForce.limit(this.maxForce)
-        }
-        return steerForce
-    }
-
-    seperation(boids){
-        let dis, steerForce
-        let total = 0
-        steerForce = createVector()
-        for(let oBoid of boids){
-            dis = dist(this.pos.x, this.pos.y, oBoid.pos.x, oBoid.pos.y)
-            if ((oBoid != this) && (dis < this.percepRad)){
-                let diff = p5.Vector.sub(this.pos, oBoid.pos)
-                diff.div(dis)
-                steerForce.add(diff)
-                total++
-            }
-        }
-        if (total > 0){
-            steerForce.div(total)
-            steerForce.setMag(this.maxSpeed)
-            steerForce.sub(this.vel)
-            steerForce.limit(this.maxForce)
-        }
-        return steerForce
-    }
+    // seperation(boids){
+    //     let dis, steerForce
+    //     let total = 0
+    //     steerForce = createVector()
+    //     for(let oBoid of boids){
+    //         dis = dist(this.pos.x, this.pos.y, oBoid.pos.x, oBoid.pos.y)
+    //         if ((oBoid != this) && (dis < this.percepRad)){
+    //             let diff = p5.Vector.sub(this.pos, oBoid.pos)
+    //             diff.div(dis)
+    //             steerForce.add(diff)
+    //             total++
+    //         }
+    //     }
+    //     if (total > 0){
+    //         steerForce.div(total)
+    //         steerForce.setMag(this.maxSpeed)
+    //         steerForce.sub(this.vel)
+    //         steerForce.limit(this.maxForce)
+    //     }
+    //     return steerForce
+    // }
 
     edges() {
         if (this.pos.x > windowWidth) {
