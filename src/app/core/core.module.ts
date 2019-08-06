2860dfa67369380a1e104c74500ce3263bebaed2
Anirudh Mungre <anirudh.mungre@uoit.net>
2019-08-06 15:44:34 -0400

    added banner component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { CoreRoutingModule } from './core-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent],
+  declarations: [HomeComponent, HeaderComponent, BannerComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
