513e67ce62a24ca91e7f8901bc226d9012382f9c
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-10-14 22:54:52 -0400

    Revert "added banner component"
    This reverts commit 2860dfa67369380a1e104c74500ce3263bebaed2.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,10 +5,9 @@ import { CoreRoutingModule } from './core-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
-import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, BannerComponent],
+  declarations: [HomeComponent, HeaderComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
