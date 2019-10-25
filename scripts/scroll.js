153724d86fcfd0a5b047077672e84e827619e7e2
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-10-25 01:13:30 -0400

    smooth transitions

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,26 +1,52 @@
-let amountScrolled = 0;
+let amountScrolled;
+let headerMinimumDimension;
 let headerImage = document.getElementById("header-image");
-let imageMinimumHeight = window.innerWidth < 500 ? 50 : 100;
+let headerNav = document.getElementById("header-nav");
+let content = document.getElementById("content");
+
+const start = () => {
+    headerMinimumDimension = window.innerWidth < 500 ? 50 : 100;
+    headerImage.style.height = `${window.innerHeight}px`;
+    headerImage.style.width = `${window.width}px`;
+    headerNav.style.height = `${headerMinimumDimension * 0.6}px`;
+    setAmountScrolled();
+    resetDistances();
+}
+
+const belowHeader = () => {
+    return amountScrolled > window.innerHeight;
+}
 
 const setAmountScrolled = () => {
     amountScrolled = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
 };
 
 const resetDistances = () => {
-    headerImage.style.borderTopRightRadius = 50/window.innerHeight * amountScrolled > 50 ? "50%" : `${50/window.innerHeight * amountScrolled}%`;
-    headerImage.style.borderBottomRightRadius = 50/window.innerHeight * amountScrolled > 50 ? "50%" : `${50/window.innerHeight * amountScrolled}%`;
-    headerImage.style.height = ((window.innerHeight - amountScrolled) < imageMinimumHeight) 
-                                ? `${imageMinimumHeight}px` : `${window.innerHeight - amountScrolled}px`;
-    headerImage.style.width = ((window.innerWidth - amountScrolled * (window.innerWidth/window.innerHeight)) < imageMinimumHeight) 
-                                ? `${imageMinimumHeight}px` : `${window.innerWidth - amountScrolled * (window.innerWidth/window.innerHeight)}px`;
+    if (belowHeader()) {
+        if (headerNav.classList.contains("animate-nav-out")) {
+            headerNav.classList.remove("animate-nav-out");
+        }
+        if (!headerNav.classList.contains("animate-nav-in")) {
+            headerNav.classList.add("animate-nav-in");
+        }
+    } else {
+        // There is a reason this if statement is different
+        if (headerNav.classList.contains("animate-nav-in") && !headerNav.classList.contains("animate-nav-out")) {
+            headerNav.classList.remove("animate-nav-in");
+            headerNav.classList.add("animate-nav-out");
+        }
+    }
+    headerImage.style.borderBottomLeftRadius = 50 / window.innerHeight * amountScrolled > 50 ? "50%" : `${50 / window.innerHeight * amountScrolled}%`;
+    headerImage.style.borderBottomRightRadius = 50 / window.innerHeight * amountScrolled > 50 ? "50%" : `${50 / window.innerHeight * amountScrolled}%`;
+    headerImage.style.height = ((window.innerHeight - amountScrolled) < headerMinimumDimension)
+        ? `${headerMinimumDimension}px` : `${window.innerHeight - amountScrolled}px`;
+    headerImage.style.width = ((window.innerWidth - amountScrolled * (window.innerWidth / window.innerHeight)) < headerMinimumDimension)
+        ? `${headerMinimumDimension}px` : `${window.innerWidth - amountScrolled * (window.innerWidth / window.innerHeight)}px`;
+    content.style.marginTop = amountScrolled + headerImage.offsetHeight > window.innerHeight ? content.style.marginTop : `${amountScrolled + headerImage.offsetHeight}px`;
 };
 
 window.addEventListener("resize", () => {
-    imageMinimumHeight = window.innerWidth < 500 ? 50 : 100;
-    headerImage.style.height = `${window.innerHeight}px`;
-    headerImage.style.width = `${window.width}px`;
-    setAmountScrolled();
-    resetDistances();
+    start();
 }, false)
 
 window.addEventListener("scroll", () => {
@@ -28,5 +54,4 @@ window.addEventListener("scroll", () => {
     resetDistances();
 }, false);
 
-setAmountScrolled();
-resetDistances();
+start();
