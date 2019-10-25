21cb0f8d3d88cb9fdf78aa991732fe5db84be6f0
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-10-24 22:40:01 -0400

    managing for different page sizes

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,23 +1,22 @@
 let amountScrolled = 0;
-let borderRadius = 0;
 let headerImage = document.getElementById("header-image");
-// headerImage.style.borderRadius = "0%";
-// headerImage.style.height = window.innerHeight;
+let imageMinimumHeight = window.innerWidth < 500 ? 50 : 100;
 
 const setAmountScrolled = () => {
     amountScrolled = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
 };
 
 const resetDistances = () => {
-    headerImage.style.borderTopRightRadius = `${50/window.innerHeight * amountScrolled}%`;
-    headerImage.style.borderBottomRightRadius = `${50/window.innerHeight * amountScrolled}%`;
-    headerImage.style.height = ((window.innerHeight - amountScrolled) < 100) 
-                                ? `100px` : `${window.innerHeight - amountScrolled}px`;
-    headerImage.style.width = ((window.innerWidth - amountScrolled * (window.innerWidth/window.innerHeight)) < 100) 
-                                ? `100px` : `${window.innerWidth - amountScrolled * (window.innerWidth/window.innerHeight)}px`;
+    headerImage.style.borderTopRightRadius = 50/window.innerHeight * amountScrolled > 50 ? "50%" : `${50/window.innerHeight * amountScrolled}%`;
+    headerImage.style.borderBottomRightRadius = 50/window.innerHeight * amountScrolled > 50 ? "50%" : `${50/window.innerHeight * amountScrolled}%`;
+    headerImage.style.height = ((window.innerHeight - amountScrolled) < imageMinimumHeight) 
+                                ? `${imageMinimumHeight}px` : `${window.innerHeight - amountScrolled}px`;
+    headerImage.style.width = ((window.innerWidth - amountScrolled * (window.innerWidth/window.innerHeight)) < imageMinimumHeight) 
+                                ? `${imageMinimumHeight}px` : `${window.innerWidth - amountScrolled * (window.innerWidth/window.innerHeight)}px`;
 };
 
 window.addEventListener("resize", () => {
+    imageMinimumHeight = window.innerWidth < 500 ? 50 : 100;
     headerImage.style.height = `${window.innerHeight}px`;
     headerImage.style.width = `${window.width}px`;
     setAmountScrolled();
