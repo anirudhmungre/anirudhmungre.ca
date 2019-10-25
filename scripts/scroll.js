56dd2c4ca7472edcd5c7c7285db9e3a4f65c4881
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-10-24 22:27:08 -0400

    awesome scrolling

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,20 +1,33 @@
-let amountScrolled;
+let amountScrolled = 0;
 let borderRadius = 0;
 let headerImage = document.getElementById("header-image");
-headerImage.style.borderRadius = "0%";
-headerImage.style.height = window.innerHeight;
+// headerImage.style.borderRadius = "0%";
+// headerImage.style.height = window.innerHeight;
 
 const setAmountScrolled = () => {
     amountScrolled = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    // console.log(amountScrolled);
-    // console.log(window.innerHeight);
-}
+};
+
+const resetDistances = () => {
+    headerImage.style.borderTopRightRadius = `${50/window.innerHeight * amountScrolled}%`;
+    headerImage.style.borderBottomRightRadius = `${50/window.innerHeight * amountScrolled}%`;
+    headerImage.style.height = ((window.innerHeight - amountScrolled) < 100) 
+                                ? `100px` : `${window.innerHeight - amountScrolled}px`;
+    headerImage.style.width = ((window.innerWidth - amountScrolled * (window.innerWidth/window.innerHeight)) < 100) 
+                                ? `100px` : `${window.innerWidth - amountScrolled * (window.innerWidth/window.innerHeight)}px`;
+};
+
+window.addEventListener("resize", () => {
+    headerImage.style.height = `${window.innerHeight}px`;
+    headerImage.style.width = `${window.width}px`;
+    setAmountScrolled();
+    resetDistances();
+}, false)
 
 window.addEventListener("scroll", () => {
-    let initialAmountScrolled = amountScrolled;
     setAmountScrolled();
-    headerImage.style.borderRadius = `${50/window.innerHeight * amountScrolled}%`;
-    // console.log(headerImage.offsetHeight);
-    // headerImage.style.height = `${window.innerHeight - amountScrolled - 100}px`
-    // headerImage.style.width = `${window.innerWidth - amountScrolled - 100}px`
+    resetDistances();
 }, false);
+
+setAmountScrolled();
+resetDistances();
