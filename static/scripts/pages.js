51e449160c61d29a7f612cecc98606ec94f429b7
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-11-15 22:11:49 -0500

    full page js

--- static/scripts/pages.js
+++ static/scripts/pages.js
@@ -1,7 +1,9 @@
 let myFullpage = new fullpage('#fullpage', {
-    // licenseKey: FULLPAGE_API_KEY,
+    licenseKey: FULLPAGE_API_KEY,
     sectionsColor: ['#212121', '#121212', '#212121', '#121212']
 });
 
 let scene = document.getElementById('scene');
-let parallaxInstance = new Parallax(scene);
+let parallaxInstance = new Parallax(scene, {
+    relativeInput: true
+});
