3e40b83322863e318919653ecd546f3be366321a
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-12-13 19:37:51 -0500

    WHY AM I STARTING AGAIN

--- static/scripts/pages.js
+++ static/scripts/pages.js
@@ -1,6 +1,6 @@
 let myFullpage = new fullpage('#fullpage', {
     licenseKey: FULLPAGE_API_KEY,
-    sectionsColor: ['#212121', '#212121', '#212121', '#212121']
+    sectionsColor: ['212121', '#212121', '#212121', '#212121']
 });
 
 let scene = document.getElementById('scene');
