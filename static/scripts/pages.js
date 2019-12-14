265d283a0752d0577a536fe4ac28e8e4b84da91a
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-12-13 19:47:46 -0500

    dont need that either

--- static/scripts/pages.js
+++
@@ -1,9 +0,0 @@
-let myFullpage = new fullpage('#fullpage', {
-    licenseKey: FULLPAGE_API_KEY,
-    sectionsColor: ['212121', '#212121', '#212121', '#212121']
-});
-
-let scene = document.getElementById('scene');
-let parallaxInstance = new Parallax(scene, {
-    relativeInput: true
-});
